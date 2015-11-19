b7469f7b365e1d2ef5bdbcd52e35c2e8b97459a5
8108_web (Student) <s_8108_web@PEDAGOGIQUE.LAN>
2015-11-19 14:28:44 +0100

    Insertion exercice 03

---
+++ exercice03/index.js
@@ -0,0 +1,18 @@
+/*var fs = require("fs");
+fs.readdir("fichiers", function (err, data) {
+  if (err) throw err;
+console.log("Il y a "+data.length+" fichiers dans ce dossier.")
+
+  for (var i = 0, l = data.length; i<l; i++) {
+    console.log(data[i]);
+  }
+});*/
+
+var fs = require("fs");
+fs.readdir("fichiers", function (err, data) {
+  if (err) throw err;
+  console.log("Il y a "+data.length+" fichiers dans ce dossier.")
+
+  data.forEach(function(elem) {
+    console.log(elem);
+});
